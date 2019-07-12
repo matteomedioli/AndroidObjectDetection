d55e56c0cafc1b0344e275d5dbe75512851ec02f
Matteo Medioli <31959430+Ickarus@users.noreply.github.com>
2019-07-12 12:46:59 +0200

    Add Welcome Activity and graphic features

---
+++ app/src/main/java/org/med/darknetandroid/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package org.med.darknetandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(WelcomeActivity.this, CameraActivity.class);
+                startActivity(welcome);
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
