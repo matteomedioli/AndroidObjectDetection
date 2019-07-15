64516408da5c70811a74f30d541a9c0bd3a5e4c3
Matteo Medioli <31959430+Ickarus@users.noreply.github.com>
2019-07-15 15:40:29 +0200

    Fix bounding boxes

--- app/src/main/java/org/med/darknetandroid/CameraActivity.java
+++ app/src/main/java/org/med/darknetandroid/CameraActivity.java
@@ -4,7 +4,10 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -12,6 +15,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +25,11 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.dnn.Dnn;
@@ -83,6 +85,8 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
             colors.add(randomColor());
     }
 
+
+
     @Override
     public void onResume() {
         super.onResume();
@@ -95,97 +99,83 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         }
     }
 
+
+
     @Override
     public void onCameraViewStarted(int width, int height) {
 
-        String modelConfiguration = getAssetsFile("yolov2-tiny.cfg", this);
-        String modelWeights = getAssetsFile("yolov2-tiny.weights", this);
+        String modelConfiguration = getAssetsFile("yolov3-tiny.cfg", this);
+        String modelWeights = getAssetsFile("yolov3-tiny.weights", this);
         net = Dnn.readNetFromDarknet(modelConfiguration, modelWeights);
     }
+
+
 
     @Override
     public void onCameraViewStopped() {
     }
 
+
+
     @Override
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame){
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
-
-        // Rotate mRgba 90 degrees
         Mat frame = inputFrame.rgba();
         Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
         Size frame_size = new Size(416, 416);
         Scalar mean = new Scalar(127.5);
 
-        Mat blob = Dnn.blobFromImage(frame, 0.00392, frame_size, mean, true, false);
+        Mat blob = Dnn.blobFromImage(frame, 1.0 / 255.0, frame_size, mean, true, false);
+        //save_mat(blob);
         net.setInput(blob);
 
         List<Mat> result = new ArrayList<>();
         List<String> outBlobNames = net.getUnconnectedOutLayersNames();
 
         net.forward(result, outBlobNames);
-        float confThreshold = 0.25f;
-        List<Integer> clsIds = new ArrayList<>();
-        List<Float> confs = new ArrayList<>();
-        List<Rect> rects = new ArrayList<>();
-        List<String> labels = new ArrayList<>();
-        List<Scalar> clrs = new ArrayList<>();
-        for (int i = 0; i < result.size(); ++i)
-        {
+        float confThreshold = 0.5f;
+
+        for (int i = 0; i < result.size(); ++i) {
             // each row is a candidate detection, the 1st 4 numbers are
             // [center_x, center_y, width, height], followed by (N-4) class probabilities
             Mat level = result.get(i);
-            for (int j = 0; j < level.rows(); ++j)
-            {
+            for (int j = 0; j < level.rows(); ++j) {
                 Mat row = level.row(j);
                 Mat scores = row.colRange(5, level.cols());
                 Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
-                float confidence = (float)mm.maxVal;
+                float confidence = (float) mm.maxVal;
                 Point classIdPoint = mm.maxLoc;
-                if (confidence > confThreshold)
-                {
+                if (confidence > confThreshold) {
 
-                    int centerX = (int)(row.get(0,0)[0] * frame.cols());
-                    int centerY = (int)(row.get(0,1)[0] * frame.rows());
-                    int width   = (int)(row.get(0,2)[0] * frame.cols());
-                    int height  = (int)(row.get(0,3)[0] * frame.rows());
-                    int left    = centerX - width  / 2;
-                    int top     = centerY - height / 2;
+                    int centerX = (int) (row.get(0, 0)[0] * frame.cols());
+                    int centerY = (int) (row.get(0, 1)[0] * frame.rows());
+                    int width = (int) (row.get(0, 2)[0] * frame.cols());
+                    int height = (int) (row.get(0, 3)[0] * frame.rows());
 
-                    clsIds.add((int)classIdPoint.x);
-                    confs.add(confidence);
+                    int left = (int) (centerX - width * 0.5);
+                    int top =(int)(centerY - height * 0.5);
+                    int right =(int)(centerX + width * 0.5);
+                    int bottom =(int)(centerY + height * 0.5);
+
+                    Point left_top = new Point(left, top);
+                    Point right_bottom=new Point(right, bottom);
+                    Point label_left_top = new Point(left, top-5);
                     DecimalFormat df = new DecimalFormat("#.##");
-                    labels.add(classNames.get(clsIds.get(clsIds.size()-1))+ ": "+df.format(confs.get(i)));
-                    clrs.add(colors.get(clsIds.get(clsIds.size()-1)));
-                    rects.add(new Rect(left, top, width, height));
+
+                    int class_id = (int) classIdPoint.x;
+                    String label= classNames.get(class_id) + ": " + df.format(confidence);
+                    Scalar color= colors.get(class_id);
+
+                    Imgproc.rectangle(frame, left_top,right_bottom , color, 3, 2);
+                    Imgproc.putText(frame, label, label_left_top, Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 0), 4);
+                    Imgproc.putText(frame, label, label_left_top, Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 255, 255), 2);
                 }
             }
         }
-
-        // Apply non-maximum suppression procedure.
-        float nmsThresh = 0.5f;
-        MatOfFloat confidences = new MatOfFloat();
-        confidences.fromList(confs);
-        Rect[] boxesArray = rects.toArray(new Rect[0]);
-        MatOfRect boxes = new MatOfRect(boxesArray);
-        MatOfInt indices = new MatOfInt();
-        Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices); //We draw the bounding boxes for objects here//
-        // Draw result boxes:
-        if(!indices.empty())
-        {
-            int [] ind = indices.toArray();
-            for (int i = 0; i < ind.length; ++i)
-            {
-                int idx = ind[i];
-                Rect box = boxesArray[idx];
-                Imgproc.rectangle(frame, box.tl(), box.br(), clrs.get(idx), 3, 2);
-                Imgproc.putText (frame, labels.get(idx), new Point(box.x, box.y-5), Imgproc.FONT_HERSHEY_SIMPLEX , 1, new Scalar(0, 0, 0),4);
-                Imgproc.putText (frame, labels.get(idx), new Point(box.x, box.y-5), Imgproc.FONT_HERSHEY_SIMPLEX , 1, new Scalar(255, 255, 255),2);
-            }
-        }
-
         return frame;
     }
+
+
 
     private boolean checkPermissions() {
 
@@ -200,6 +190,7 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         }
 
     }
+
 
 
     private static String getAssetsFile(String file, Context context) {
@@ -223,6 +214,8 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         }
         return "";
     }
+
+
 
     private List<String> readLabels (String file, Context context)
     {
@@ -253,6 +246,8 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         return labelsArray;
     }
 
+
+
     private Scalar randomColor() {
         Random random = new Random();
         int r = random.nextInt(255);
@@ -260,6 +255,31 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         int b = random.nextInt(255);
         return new Scalar(r,g,b);
     }
+
+
+
+    private void save_mat(Mat mat)
+    {
+        String path = Environment.getExternalStorageDirectory().toString();
+        OutputStream fOut = null;
+        File file = new File(path, "screen.jpg"); // the File to save , append increasing numeric counter to prevent files from getting overwritten.
+        try {
+            Bitmap bmp = Bitmap.createBitmap(mat.width(),mat.height(), Bitmap.Config.ARGB_8888);
+            Mat tmp = new Mat (mat.width(),mat.height(), CvType.CV_8UC1,new Scalar(4));
+            Imgproc.cvtColor(mat, tmp, Imgproc.COLOR_RGB2BGRA);
+            //Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+            Utils.matToBitmap(tmp, bmp);
+            fOut = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
+            fOut.flush(); // Not really required
+            fOut.close(); // do not forget to close the stream
+            MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 
     @Override
